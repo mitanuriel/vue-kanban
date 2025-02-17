98d80a9507334d72c7719621a29e6a110ec1cae9
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-02-18 00:37:42 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,22 @@
-<script>
 
-export default {
-  name: "App",
-  components: "",
-
-};
-data(){
-return {
-  newTask: "",
-  arrBacklog: [
-    {name: "Code Sign Up Page"},
-    {name: "Test Dashboard"},
-    {name: "Style Register"},
-    {name: "Help page"},
-  ]
-}
-}
-</script>
 
 <template>
-
-
-  <RouterView />
+  <v-app>
+    <v-container>
+      <h1 class="text-center">Kanban Board</h1>
+      <KanbanBoard />
+    </v-container>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import KanbanBoard from './components/KanbanBoard.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+<style>
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+h1 {
+  margin-top: 20px;
 }
 </style>

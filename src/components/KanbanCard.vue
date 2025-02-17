b8d6e740656eeb9be3e2130e47fb58a7a84ac703
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-02-17 23:38:16 +0100

    KanbanCard

---
+++ src/components/KanbanCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="mb-2 pa-2 ">
+    <v-card-title> {{ card.title }}</v-card-title>
+    <v-card-subtitle> {{ card.description }}</v-card-subtitle>
+
+    <v-btn-group class="mt-2" dense>
+      <v-btn
+      v-for="option in moveOptions"
+      :key="option.status"
+      @click="moveCard(option.status)"
+      small
+      >
+      Move to {{ option.title }}
+    </v-btn>
+    </v-btn-group>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, computed } from 'vue';
+
+defineProps<{
+  card: { id:number; title: string; description: string; status: string};
+}>();
+
+const emit = defineEmits<{
+(event: 'move-card', cardId: number, newStatus: string): void;
+}>();
+
+const moveOptions = computed(() => {
+   const statuses = [ 'todo', 'in-progress', 'done'];
+   return statuses
+   .filter (status => status !== card.status)
+   .map(status =>({ status, title: status.replace('-', '')}));
+});
+
+//function to move the card
+
+const moveCard = (newStatus: string) => {
+  emit('move-card', card.id, newStatus);
+};
+</script>

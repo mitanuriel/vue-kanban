b59fdfce607033c1040a4fe555064aad59f89827
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-02-17 20:55:20 +0100

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify;
 
-app.use(router)
 
-app.mount('#app')
+app.use(router);
+
+app.use(vuetify);
+app.mount('#app');

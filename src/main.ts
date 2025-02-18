a6d598d7c9c152c2573b9477db89b69f60a85b2b
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-02-18 14:46:20 +0100

    uninstalled unwanted dependencies (bootstrap)

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles'
-import router from './router'
 
 //import './assets/main.css';
 //import './assets/base.css';
 
-const app = createApp(App)
 const vuetify = createVuetify;
+const app = createApp(App)
 
 
-app.use(router);
 
 app.use(vuetify);
 app.mount('#app');

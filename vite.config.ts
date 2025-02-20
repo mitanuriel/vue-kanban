2e2c8f1c0061660bd98ab1ad2fc79f855389a455
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-02-20 11:59:23 +0100

    Added base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/vue-kanban/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
